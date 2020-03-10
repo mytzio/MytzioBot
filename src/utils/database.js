91bc504f897b635f5f8b310c29c45a629e267495
Teo Temonen <teo.temonen@protonmail.com>
2020-03-10 12:57:26 +0200

    Removed unnecessary variables

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,20 +3,9 @@ const mongoose = require('mongoose');
 const Guild = new mongoose.Schema({
 	_id: String,
 	prefix: { type: String, default: '!' },
-	activityRoles: [{
-		name: String,
-		type: String,
-	}],
-	reactionRoles: [{
-		channel: String,
-		message: String,
-		reaction: String,
-		role: String,
-	}],
 	mediaPlayer: {
 		volume: { type: Number, default: 100 },
 	},
-	autoRoles: [ String ],
 });
 
 module.exports = {
