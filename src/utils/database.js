5593a9b79d0c9ffe5c8ad6e3debe7582549a7353
Teo Temonen <teo.temonen@protonmail.com>
2020-03-21 16:04:44 +0200

    add locale

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Guild = new mongoose.Schema({
 	_id: String,
 	prefix: { type: String, default: '!' },
+	locale: { type: String, default: 'fi' },
 	mediaPlayer: {
 		volume: { type: Number, default: 100 },
 	},
