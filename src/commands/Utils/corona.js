e9a7ffe3eb8649a6cbd02e48309116ff66fa7cd5
Teo Temonen <teo.temonen@protonmail.com>
2020-03-23 15:03:51 +0200

    Cleanup code

--- src/commands/Utils/corona.js
+++ src/commands/Utils/corona.js
@@ -25,60 +25,70 @@ module.exports.run = async (client, message) => {
 		const deaths = response.data.deaths;
 		const recovered = response.data.recovered;
 
-		let lastConfirmed = confirmed.slice(-1);
-		try {
-			lastConfirmed[0].date = dayjs(lastConfirmed[0].date).utc(0o200).locale(guild.locale).format('L LT');
-			lastConfirmed = `**${lastConfirmed[0].healthCareDistrict != null ? lastConfirmed[0].healthCareDistrict : 'unknown'}**\n*${lastConfirmed[0].date}*`;
-		}
-		catch (e) {
-			lastConfirmed = '-';
-		}
+		const date = (time) => {
+			return dayjs(time).utc(0o200).locale(guild.locale).format('L');
+		};
 
-		let lastDeath = deaths.slice(-1);
-		try {
-			lastDeath[0].date = dayjs(lastDeath[0].date).utc(0o200).locale(guild.locale).format('L LT');
-			lastDeath = `**${lastDeath[0].healthCareDistrict != null ? lastDeath[0].healthCareDistrict : 'unknown'}**\n*${lastDeath[0].date}*`;
-		}
-		catch (e) {
-			lastDeath = '-';
-		}
+		const healthCareDistrict = (district) => {
+			if (district === null || district.length < 1) {
+				return 'unknown';
+			}
+			else {
+				return district;
+			}
+		};
 
-		let lastRecovered = recovered.slice(-1);
-		try {
-			lastRecovered[0].date = dayjs(lastRecovered[0].date).utc(0o200).locale(guild.locale).format('L LT');
-			lastRecovered = `**${lastRecovered[0].healthCareDistrict != null ? lastRecovered[0].healthCareDistrict : 'unknown'}**\n*${lastRecovered[0].date}*`;
-		}
-		catch (e) {
-			lastRecovered = '-';
-		}
+		const lastCase = (object) => {
+			let last = object.slice(-1)[0];
+			try {
+				last.date = date(last.date);
+				last.healthCareDistrict = healthCareDistrict(last.healthCareDistrict);
+				return last = `**${last.healthCareDistrict}**\n*${last.date}*`;
+			}
+			catch (e) {
+				return last = '-';
+			}
+		};
 
 		const embed = new MessageEmbed()
 			.setTitle('Corona in Finland')
 			.setDescription('Statistics about COVID-19 in Finland')
-			.addField('Infected', confirmed.length, true)
+			.addField('Confirmed', confirmed.length, true)
 			.addField('Deaths', deaths.length, true)
 			.addField('Recovered', recovered.length, true)
 			.addField('\u200B', '\u200B')
-			.addField('Last Infected', lastConfirmed, true)
-			.addField('Last Death', lastDeath, true)
-			.addField('Last Recovered', lastRecovered, true)
+			.addField('Last Confirmed', lastCase(confirmed), true)
+			.addField('Last Death', lastCase(deaths), true)
+			.addField('Last Recovered', lastCase(recovered), true)
 			.addField('\u200B', '\u200B')
 			.setFooter(`Replying to ${message.author.tag} - Sources: https://github.com/HS-Datadesk/koronavirus-avoindata`);
 
-		const districtsGrouped = _.groupBy(confirmed, d => d.healthCareDistrict);
-		const districtsGroupedArr = _.toArray(districtsGrouped);
-		const sorted = _.sortBy(districtsGroupedArr, d => d.length);
-		sorted.reverse();
+		await confirmed.forEach(item => {
+			if (item.healthCareDistrict < 1) item.healthCareDistrict = null;
+		});
+
+		const districtsGrouped = _.groupBy(confirmed, district => district.healthCareDistrict);
+		const sortedDescending = _.sortBy(districtsGrouped, district => district.length).reverse();
+
 		let counter = 0;
 		for (let i = 0; i < 8; i++) {
-			const item = sorted[i];
-			embed.addField(item[0].healthCareDistrict ? item[0].healthCareDistrict : 'unknown', item.length, true);
+			const item = sortedDescending[i];
+			embed.addField(healthCareDistrict(item[0].healthCareDistrict), item.length, true);
 			counter += item.length;
 		}
 
 		embed.addField('Other', confirmed.length - counter, true);
 
 		message.channel.send(embed);
+
+		const activity = () => {
+			client.user.setActivity(`C: ${confirmed.length}, D: ${deaths.length}, R: ${recovered.length}`, { type: 'WATCHING' });
+		};
+
+		activity();
+		setInterval(() => {
+			activity();
+		}, 300000);
 	}
 	catch (e) {
 		logger.error(e);
